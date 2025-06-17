482b93314fbe06c747cdb0a3b590f3b7c060afca
Christopher Mackay <cmackay732@gmail.com>
2025-06-16 20:25:15 -0400

    Added sliders for value and weight

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,18 @@ st.write('Filter and search all ingredients and effects in The Elder Scrolls V: 
 
 df = pd.read_csv('ingredients.csv')
 
-search_str = st.text_input("Filter ingredients and effects", "")
+search_str = st.text_input("Filter ingredients", "")
 
 mask = df.apply(lambda row: row.astype(str).str.contains(search_str, case=False, na=False)).any(axis=1)
-filtered_df = df[mask]
+df = df[mask]
 
-st.write(filtered_df)
+values = st.slider("Select a value range", 0.0, 15.0, (0.0, 15.0))
+st.write("Value range", values)
 
+weights = st.slider("Select a weight range", 0.0, 0.5, (0.0, 0.5))
+st.write("Weight range", weights)
+
+df = df[df["Value"].between(values[0], values[1])]
+df = df[df["Weight"].between(weights[0], weights[1])]
+
+st.write(df)
