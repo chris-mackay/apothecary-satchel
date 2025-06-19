794a3d436765230bc809c284292285272967d901
Christopher Mackay <cmackay732@gmail.com>
2025-06-19 19:27:01 -0400

    Added values and weights to skyrim

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,31 +7,30 @@ st.write('Filter and search all ingredients and effects in the following Elder S
 col1, col2, col3, col4 = st.columns([1,1,1,1])
 
 with col1:
-    st.link_button("The Elder Scrolls V: Skyrim", "https://en.uesp.net/wiki/Skyrim:Ingredients")
+    st.link_button("The Elder Scrolls III: Morrowind", "https://en.uesp.net/wiki/Morrowind:Ingredients")
 
 with col2:
     st.link_button("The Elder Scrolls IV: Oblivion", "https://en.uesp.net/wiki/Oblivion:Ingredients")
 
 with col3:
-    st.link_button("The Elder Scrolls III: Morrowind", "https://en.uesp.net/wiki/Morrowind:Ingredients")
-    
+    st.link_button("The Elder Scrolls V: Skyrim", "https://en.uesp.net/wiki/Skyrim:Ingredients")
+
 with col4:
     st.link_button("The Elder Scrolls Online", "https://eso-hub.com/en/alchemy-reagents-and-solvents")
 
-
 game = st.radio(
     "Select game to view ingredients",
-    ["The Elder Scrolls V: Skyrim", "The Elder Scrolls IV: Oblivion", "The Elder Scrolls III: Morrowind", "The Elder Scrolls Online"]
+    ["The Elder Scrolls III: Morrowind", "The Elder Scrolls IV: Oblivion", "The Elder Scrolls V: Skyrim", "The Elder Scrolls Online"]
 )
 
-if game == "The Elder Scrolls V: Skyrim":
-    df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_skyrim.csv")
+if game == "The Elder Scrolls III: Morrowind":
+    df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_morrowind.csv")
     search_str = st.text_input("Filter ingredients", "")
 elif game == "The Elder Scrolls IV: Oblivion":
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_oblivion.csv")
     search_str = st.text_input("Filter ingredients", "")
-elif game == "The Elder Scrolls III: Morrowind":
-    df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_morrowind.csv")
+elif game == "The Elder Scrolls V: Skyrim":
+    df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_skyrim.csv")
     search_str = st.text_input("Filter ingredients", "")
 else:
     st.link_button("Alchemy Simulator", "https://eso-hub.com/en/potion-maker")
