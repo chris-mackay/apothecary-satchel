7e934e8d81ae798ed58f1399e1c19f2872866df2
chris-mackay <cmackay732@gmail.com>
2025-06-21 19:09:28 -0400

    Added link button for daggerfall, reorganized columns, and added max weight and value variables.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,51 @@
 import streamlit as st
 import pandas as pd
 
+WEIGHT_MAX_DAGGERFALL = 3.0
 WEIGHT_MAX_MORROWIND = 50.0
 WEIGHT_MAX_OBLIVION = 5.0
 WEIGHT_MAX_SKYRIM = 1.0
 
+VALUE_MAX_DAGGERFALL = 500.0
 VALUE_MAX_MORROWIND = 500.0
 VALUE_MAX_OBLIVION = 75.0
 VALUE_MAX_SKYRIM = 250.0
 
 st.title('Apothecary Satchel')
-st.write('Filter and search all ingredients and effects in the following Elder Scrolls games')
+st.write('Filter and search all reagents and effects in the main line Elder Scrolls games')
 
-col1, col2, col3, col4 = st.columns([1,1,1,1])
+col1, col2, col3 = st.columns([1.08,1,1])
 
 with col1:
+    st.link_button("The Elder Scrolls II: Daggerfall", "https://en.uesp.net/wiki/Daggerfall:Ingredients")
     st.link_button("The Elder Scrolls III: Morrowind", "https://en.uesp.net/wiki/Morrowind:Ingredients")
 
 with col2:
     st.link_button("The Elder Scrolls IV: Oblivion", "https://en.uesp.net/wiki/Oblivion:Ingredients")
-
-with col3:
     st.link_button("The Elder Scrolls V: Skyrim", "https://en.uesp.net/wiki/Skyrim:Ingredients")
 
-with col4:
+with col3:
     st.link_button("The Elder Scrolls Online", "https://eso-hub.com/en/alchemy-reagents-and-solvents")
 
 game = st.radio(
     "Select game to view ingredients",
-    ["The Elder Scrolls III: Morrowind", "The Elder Scrolls IV: Oblivion", "The Elder Scrolls V: Skyrim", "The Elder Scrolls Online"]
+    ["The Elder Scrolls II: Daggerfall", "The Elder Scrolls III: Morrowind", "The Elder Scrolls IV: Oblivion", "The Elder Scrolls V: Skyrim", "The Elder Scrolls Online"]
 )
 
-if game == "The Elder Scrolls III: Morrowind":
+if game == "The Elder Scrolls II: Daggerfall":
+    df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_daggerfall.csv")
+elif game == "The Elder Scrolls III: Morrowind":
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_morrowind.csv")
-    search_str = st.text_input("Filter ingredients", "")
 elif game == "The Elder Scrolls IV: Oblivion":
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_oblivion.csv")
-    search_str = st.text_input("Filter ingredients", "")
 elif game == "The Elder Scrolls V: Skyrim":
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_skyrim.csv")
-    search_str = st.text_input("Filter ingredients", "")
 else:
     st.link_button("Alchemy Simulator", "https://eso-hub.com/en/potion-maker")
     st.link_button("Market Trade Center", "https://eso-hub.com/en/trading?category=4&sort=last_seen_at&sortdir=desc&server=NA")
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_eso.csv")
-    search_str = st.text_input("Filter ingredients", "")
+
+search_str = st.text_input("Filter ingredients", "")
 
 def highlight_match(val):
     if search_str.lower() in str(val).lower():
@@ -52,7 +53,18 @@ def highlight_match(val):
     return ''
 
 if game != "The Elder Scrolls Online":
-    if game == "The Elder Scrolls III: Morrowind":
+    if game == "The Elder Scrolls II: Daggerfall":
+        weights = st.slider("Select a weight range", 0.0, WEIGHT_MAX_DAGGERFALL, (0.0, WEIGHT_MAX_DAGGERFALL))
+        st.write("Weight range", weights)
+
+        values = st.slider("Select a value range", 0.0, VALUE_MAX_DAGGERFALL, (0.0, VALUE_MAX_DAGGERFALL))
+        st.write("Value range", values)
+
+        df = df[df["Weight"].between(weights[0], weights[1])]
+        df = df[df["Value"].between(values[0], values[1])]
+
+
+    elif game == "The Elder Scrolls III: Morrowind":
         weights = st.slider("Select a weight range", 0.0, WEIGHT_MAX_MORROWIND, (0.0, WEIGHT_MAX_MORROWIND))
         st.write("Weight range", weights)
 
