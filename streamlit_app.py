1227088469cae557b3f333568c9e3e87e3860c3d
chris-mackay <cmackay732@gmail.com>
2025-06-21 21:13:29 -0400

    Refactored game names into constants

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+DAGGERFALL = "The Elder Scrolls II: Daggerfall"
+MORROWIND = "The Elder Scrolls III: Morrowind"
+OBLIVION = "The Elder Scrolls IV: Oblivion"
+SKYRIM = "The Elder Scrolls V: Skyrim"
+ELDER_SCROLLS_ONLINE = "The Elder Scrolls Online"
+
 WEIGHT_MAX_DAGGERFALL = 3.0
 WEIGHT_MAX_MORROWIND = 50.0
 WEIGHT_MAX_OBLIVION = 5.0
@@ -17,30 +23,38 @@ st.write('Filter and search all reagents and effects in the main line Elder Scro
 col1, col2, col3 = st.columns([1.08,1,1])
 
 with col1:
-    st.link_button("The Elder Scrolls II: Daggerfall", "https://en.uesp.net/wiki/Daggerfall:Ingredients")
-    st.link_button("The Elder Scrolls III: Morrowind", "https://en.uesp.net/wiki/Morrowind:Ingredients")
+    st.link_button(DAGGERFALL, "https://en.uesp.net/wiki/Daggerfall:Ingredients")
+    st.link_button(MORROWIND, "https://en.uesp.net/wiki/Morrowind:Ingredients")
 
 with col2:
-    st.link_button("The Elder Scrolls IV: Oblivion", "https://en.uesp.net/wiki/Oblivion:Ingredients")
-    st.link_button("The Elder Scrolls V: Skyrim", "https://en.uesp.net/wiki/Skyrim:Ingredients")
+    st.link_button(OBLIVION, "https://en.uesp.net/wiki/Oblivion:Ingredients")
+    st.link_button(SKYRIM, "https://en.uesp.net/wiki/Skyrim:Ingredients")
 
 with col3:
-    st.link_button("The Elder Scrolls Online", "https://eso-hub.com/en/alchemy-reagents-and-solvents")
+    st.link_button(ELDER_SCROLLS_ONLINE, "https://eso-hub.com/en/alchemy-reagents-and-solvents")
 
 game = st.radio(
     "Select game to view ingredients",
-    ["The Elder Scrolls II: Daggerfall", "The Elder Scrolls III: Morrowind", "The Elder Scrolls IV: Oblivion", "The Elder Scrolls V: Skyrim", "The Elder Scrolls Online"]
+    [DAGGERFALL, 
+     MORROWIND, 
+     OBLIVION, 
+     SKYRIM, 
+     ELDER_SCROLLS_ONLINE]
 )
 
-if game == "The Elder Scrolls II: Daggerfall":
+if game == DAGGERFALL:
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_daggerfall.csv")
-elif game == "The Elder Scrolls III: Morrowind":
+
+elif game == MORROWIND:
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_morrowind.csv")
-elif game == "The Elder Scrolls IV: Oblivion":
+
+elif game == OBLIVION:
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_oblivion.csv")
-elif game == "The Elder Scrolls V: Skyrim":
+
+elif game == SKYRIM:
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_skyrim.csv")
-else:
+
+elif game == ELDER_SCROLLS_ONLINE:
     st.link_button("Alchemy Simulator", "https://eso-hub.com/en/potion-maker")
     st.link_button("Market Trade Center", "https://eso-hub.com/en/trading?category=4&sort=last_seen_at&sortdir=desc&server=NA")
     df = pd.read_csv("https://raw.githubusercontent.com/chris-mackay/apothecary-satchel/refs/heads/main/data_eso.csv")
@@ -52,47 +66,37 @@ def highlight_match(val):
         return 'background-color: rgba(139,230,139,0.4)'
     return ''
 
-if game != "The Elder Scrolls Online":
-    if game == "The Elder Scrolls II: Daggerfall":
+if game != ELDER_SCROLLS_ONLINE:
+    if game == DAGGERFALL:
         weights = st.slider("Select a weight range", 0.0, WEIGHT_MAX_DAGGERFALL, (0.0, WEIGHT_MAX_DAGGERFALL))
         st.write("Weight range", weights)
 
         values = st.slider("Select a value range", 0.0, VALUE_MAX_DAGGERFALL, (0.0, VALUE_MAX_DAGGERFALL))
         st.write("Value range", values)
 
-        df = df[df["Weight"].between(weights[0], weights[1])]
-        df = df[df["Value"].between(values[0], values[1])]
-
-
-    elif game == "The Elder Scrolls III: Morrowind":
+    elif game == MORROWIND:
         weights = st.slider("Select a weight range", 0.0, WEIGHT_MAX_MORROWIND, (0.0, WEIGHT_MAX_MORROWIND))
         st.write("Weight range", weights)
 
         values = st.slider("Select a value range", 0.0, VALUE_MAX_MORROWIND, (0.0, VALUE_MAX_MORROWIND))
         st.write("Value range", values)
 
-        df = df[df["Weight"].between(weights[0], weights[1])]
-        df = df[df["Value"].between(values[0], values[1])]
-
-    elif game == "The Elder Scrolls IV: Oblivion":
+    elif game == OBLIVION:
         weights = st.slider("Select a weight range", 0.0, WEIGHT_MAX_OBLIVION, (0.0, WEIGHT_MAX_OBLIVION))
         st.write("Weight range", weights)
 
         values = st.slider("Select a value range", 0.0, VALUE_MAX_OBLIVION, (0.0, VALUE_MAX_OBLIVION))
         st.write("Value range", values)
 
-        df = df[df["Weight"].between(weights[0], weights[1])]
-
-        df = df[df["Value"].between(values[0], values[1])]
-    elif game == "The Elder Scrolls V: Skyrim":
+    elif game == SKYRIM:
         weights = st.slider("Select a weight range", 0.0, WEIGHT_MAX_SKYRIM, (0.0, WEIGHT_MAX_SKYRIM))
         st.write("Weight range", weights)
 
         values = st.slider("Select a value range", 0.0, VALUE_MAX_SKYRIM, (0.0, VALUE_MAX_SKYRIM))
         st.write("Value range", values)
 
-        df = df[df["Weight"].between(weights[0], weights[1])]
-        df = df[df["Value"].between(values[0], values[1])]
+    df = df[df["Weight"].between(weights[0], weights[1])]
+    df = df[df["Value"].between(values[0], values[1])]
 
 mask = df.apply(lambda row: row.astype(str).str.contains(search_str, case=False, na=False)).any(axis=1)
 df = df[mask]
